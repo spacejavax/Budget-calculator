72a0a6adcbf3f548d9e92d790d774f2ce3a5e7b2
spacejavax <zhonglile08@gmail.com>
2026-07-22 22:30:41 +0200

    moneyAfterExpenses variable on line 84
    I defined moneyAfterexpenses, and replaced {Number(monthlyIncome || 0) - Number(monthlyExpenses || 0)} kr with it

--- src/App.jsx
+++ src/App.jsx
@@ -5,49 +5,47 @@ function App() {
   const [monthlyIncome, setMonthlyIncome] = useState('')
   const [monthlyExpenses, setMonthlyExpenses] = useState('')
   const [targetAmount, setTargetAmount] = useState('')
-  const [savedAmount, setsavedAmount] = useState('')
+  const [savedAmount, setSavedAmount] = useState('')
   const remainingAmount = Math.max(
-  Number(targetAmount || 0) - Number(savedAmount || 0),
-  0
+    Number(targetAmount || 0) - Number(savedAmount || 0),
+    0
 )
 
-const moneyAfterExpenses =
-  Number(monthlyIncome || 0) - Numer(monthlyExpenses || 0)
-  0
+  const moneyAfterExpenses =
+    Number(monthlyIncome || 0) - Number(monthlyExpenses || 0)
+
   return (
     <main>
       <h1>Mitt sparmål</h1>
       <p className="description">
         Planera ditt sparmål och följ dina framsteg. </p>
       <div className="input-group">
-        <label htmlFor="monthlyIncome">
-          Inkomst per månad
-        </label>
-
+      <label htmlFor="monthlyIncome">Inkomst per månad
+      </label>
       <input
         id="monthlyIncome"
         type="number"
         min="0"
-        placeholder="Exempel: 5000"
+        placeholder=""
         value={monthlyIncome}
         onChange={(event) => setMonthlyIncome(event.target.value)}
-      />
-
+          /> 
       </div>
 
-      <p>Din månadsinkomst: {monthlyIncome || 0} kr</p>
-      <label htmlFor="monthlyExpenses">
-        Utgifter per månad
-      </label>
+      <div className="input-group">
+      <label htmlFor="monthlyExpenses">Utgifter per månad</label>
 
       <input 
       id="monthlyExpenses"
       type="number"
       min="0"
-      placeholder="Exempel: 1000"
+      placeholder=""
       value={monthlyExpenses}
       onChange={(event) => setMonthlyExpenses(event.target.value)}
     />
+      </div>
+
+    <div className="input-group">
     <label htmlFor="targetAmount">
       Hur mycket vill du spara?
     </label>
@@ -56,10 +54,13 @@ const moneyAfterExpenses =
     id="targetAmount"
     type="number"
     min="0"
-    placeholder="Exempel 1000"
+    placeholder=""
     value={targetAmount}
     onChange={(event) => setTargetAmount(event.target.value)}
     />
+  </div>
+
+  <div className="">
 
     <label htmlFor="savedAmount">
       Hur mycket har du redan sparat?
@@ -69,20 +70,18 @@ const moneyAfterExpenses =
       id="savedAmount"
       type="number"
       min="0"
-      placeholder="Exempel: 1000"
+      placeholder=""
       value={savedAmount}
-      onChange={(event) => setsavedAmount(event.target.value)}
+      onChange={(event) => setSavedAmount(event.target.value)}
     />
+  </div>
       
 
     <p className="result">
       Du har {remainingAmount} kr kvar till ditt sparmål
     </p>
 
-    <p>
-      Kvar efter utgifter:{' '}
-      {Number(monthlyIncome || 0) - Number(monthlyExpenses || 0)} kr
-    </p>
+    <p>Kvar efter utgifter: {moneyAfterExpenses} kr </p>
     </main>
   )
 }
