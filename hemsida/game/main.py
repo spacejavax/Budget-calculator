d1c11054f7dd70d90094c957e3abf3ffa41cd314
XecilA <alicexiong2008@icloud.com>
2026-07-23 22:37:45 +0200

    added the snake so that it moves and eats apples

--- hemsida/game/main.py
+++ hemsida/game/main.py
@@ -3,17 +3,20 @@ import random
 
 pygame.init()
 
+font = pygame.font.SysFont('cooperblack', 30)
 radius = 10
 screen = pygame.display.set_mode((400, 400))
 
-square = pygame.Rect(10, 200, 25, 25)
+size = 15
+
+snake = [pygame.Rect(10, 200, size, size)]
 
 darkpink = (197, 61, 105)
 white = (255, 255, 255)
 running = True
 
 clock = pygame.time.Clock()
-apple = pygame.Rect(random.randint(20, 380), random.randint(20, 380), 10, 10)
+apple = pygame.Rect(random.randint(20, 380), random.randint(20, 380), 5, 5)
 
 vel = 8
 directionx = 1
@@ -43,22 +46,35 @@ while running:
         directionx = 0
         directiony = 1
 
-    square.x += directionx*vel
-    square.y += directiony*vel
+    
+    oldposition = []
+    
+    for i in snake:
+        oldposition.append(i.topleft)
 
+    snake[0].x += directionx*vel
+    snake[0].y += directiony*vel
+
+    for i in range(1, len(snake)):
+        snake[i].topleft = oldposition[i-1]
     #eats the dot and grows 
-    if square.collidepoint(apple.center):
+    if snake[0].collidepoint(apple.center):
         pygame.time.delay(10)
         points += 1
         apple = pygame.Rect(random.randint(20, 380), random.randint(20, 380), 10, 10)
 
-    if square.left <= 0 or square.right >= 400 or square.top <= 0 or square.bottom >= 400:
+        snake.append(pygame.Rect(oldposition[-1][0], oldposition[-1][1], size, size))
+
+
+    if snake[0].left <= 0 or snake[0].right >= 400 or snake[0].top <= 0 or snake[0].bottom >= 400:
         running = False
     
     screen.fill((255, 197, 211))
 
-    pygame.draw.circle(screen, darkpink, apple.center, 10)
-    pygame.draw.rect(screen, white, square)
+    pygame.draw.circle(screen, darkpink, apple.center, 5)
+
+    for i in snake:
+        pygame.draw.rect(screen, white, i)
 
     clock.tick(25)
     pygame.display.flip()
