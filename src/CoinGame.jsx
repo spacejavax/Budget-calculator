304f3e95270f855428c5867af0c5daed671cce6d
spacejavax <zhonglile08@gmail.com>
2026-07-23 23:39:06 +0200

    new coin game
    ive started to make the first game, and my friend is working on another game ive created a function called coingame and the olayer can move left and right,.
    
    then i will create a return value

--- src/CoinGame.jsx
+++ src/CoinGame.jsx
@@ -1,7 +1,21 @@
 function CoinGame() {
     return(
         <section className="coin-game">
-        <h2>Cath</h2>
+        <h2>Catch</h2>
         </section>
     )
 }
+
+function CoinGame()
+    const[player1, setplayer1] = useState(50)
+
+    function moveLeft() {
+        setplayer1((currentPosition) =>
+            Math.max(currentPosition - 10, 5)) }
+    function moveright() {
+        setplayer1((currentPosition) =>
+        Math.min(currentPosition + 10, 95))
+    }
+
+    return (
+       
