28458941137c95e1588e00e0ad4e224e122fa207
spacejavax <zhonglile08@gmail.com>
2026-07-24 18:43:23 +0200

    commit

--- src/CoinGame.jsx
+++ src/CoinGame.jsx
@@ -1,7 +1,18 @@
 function CoinGame() {
     return(
         <section className="coin-game">
+<<<<<<< Updated upstream
         <h2>Catch</h2>
+=======
+        <h2>Catch the Coins</h2>
+
+        <p>Points: 0</p>
+        <p>Lives: ❤️❤️❤️</p>
+        <div className="game-area">
+            <div className="coin">🪙</div>
+            <div className="player">🌸</div>
+        </div>
+>>>>>>> Stashed changes
         </section>
     )
 }
