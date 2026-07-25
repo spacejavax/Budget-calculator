f37aa745b292f0451438bae5990ec69a0c372b01
spacejavax <zhonglile08@gmail.com>
2026-07-25 13:24:59 +0200

    Update CoinGame.jsx
    coingame function is created. for the game, different variables are created such as coiny (usestate is 0 so it starts falling from top).  then function mousemove is created. Event contain the information how the mouse is moving. Math max and min make sure user cannot move out of the game area.  the setcoiny is a function that returns 0 if the coin reaches the end. setinterval makes it llok like a loop.

--- src/CoinGame.jsx
+++ src/CoinGame.jsx
@@ -1,15 +1,54 @@
 import {useEffect, useState} from 'react'
 
-
+function CoinGame() {
 // All variables needed in the game
-const [playerx, set playerx] = useState(50)
+const [playerx, setplayerX] = useState(50)
 const [coinx, setcoinx] = useState(50)
-const [coiny, setcoiny] = useState(0)
+const [coiny, setcoinY] = useState(0)
 const [lives, setlives] = useState(3)
 const [score, setscore] = useState(0)
 const [gameover, setgameover] = useState(false)
 
-// The players position 
-function moveLeft() 
-    setplayerx((currentx)) => Math.max(currentx - 10, 5)
+// The players position; can move with mouse
+function handleMouseMove(event) {
+    const gameArea = event.currentTarget.getBoundingClientRect()
+    const mouseXInsideGameArea = event.clientX - gameArea.left
+    const newplayerX = (mouseX / gameArea.width) * 100
+    const clampedX= Math.max(5, Math.min(newplayerX, 95))
+    setplayerx(clampedX)
+}
+
+useEffect(()) => {
+    const fallInterval = setInterval (() => {
+        setcoinY((currentY) => {
+            const hasReachedBottom = currentY > 90
+            if (hasReachedBottom) {
+                const randomX = Math.floor(Math.random() * 80) + 10 //+ 10 bc interval should be from 10-90, preventing the coin to exist almost outside the game area
+                setcoinx(randomX)
+                return 0
+            }
+        } 
+    })
+}
+retun (
+
+    <div
+    className="coin"
+    style = {{
+    left: `${coinx}%`,
+    top: `${coiny}%`
+    }}
+>
+    🪙  
+</div>
+<div
+    className="player"
+    style={{left: `${playerx}%`}}
+>
+(˶˃ ᵕ ˂˶) 
+</div>
+</div>
+)
+}
+
 
