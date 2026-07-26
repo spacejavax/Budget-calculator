747f1222a0d487906ce19f71ee49f6e3bc4619cd
XecilA <alicexiong2008@icloud.com>
2026-07-26 20:42:14 +0200

    Create main.py

---
+++ game2/main.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+
+WIDTH = 600
+HEIGHT = 700
+
+screen = pygame.display.set_mode(WIDTH, HEIGHT)
+
+clock = pygame.time.Clock()
+
+BACKGROUND=(245, 225, 235)
+EMPTY_CELL = ()
